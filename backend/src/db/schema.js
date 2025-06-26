760a382022ae66833922731e22603ef6b763cbf9
Zack Underwood <Underwoodzack159@gmail.com>
2025-06-25 19:44:53 -0700

    Made database with DB and neon / drizzle

---
+++ backend/src/db/schema.js
@@ -0,0 +1,15 @@
+import {pgTable, serial, text, timestamp, integer} from 'drizzle-orm/pg-core';
+
+export const favorites = pgTable("favorites", {
+    id: serial("id").primaryKey(),
+    userId: text("user_id").notNull(),
+    recipeId: integer("recipe_id").notNull(),
+    title: text("title").notNull(),
+    image: text("image"),
+    cookTime: text("cook_time").notNull(),
+    servings: text("servings").notNull(),
+    createdAt: timestamp("created_at").defaultNow(),
+
+    
+
+})
